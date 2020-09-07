6ab6c988c0f31aba41dfb7cde7b9a45ca87514df
smhrd <smhrd@DESKTOP-FSD7SG4>
2020-09-07 16:44:14 +0900

    100까지

--- webmvc/src/kr/smhrd/MyUtil.java
+++ webmvc/src/kr/smhrd/MyUtil.java
@@ -1,11 +1,11 @@
 package kr.smhrd;
 
 public class MyUtil {
-	// 1 ~ 10 한글수정
+	// 1 ~ 100 한글수정
 	public int hap() {
 		// 살려주세요
 		int sum = 0;
-		for(int i = 0; i <= 10; i++) {
+		for(int i = 0; i <= 100; i++) {
 			sum +=i;
 		}
 		return sum;
